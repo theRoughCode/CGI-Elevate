e381b6d503b4734ef4a6874980374ee9849f4479
raphael <raph.koh@gmail.com>
2017-05-12 17:22:55 -0400

    Implement image upload and retrieval

--- helpers/data.js
+++ helpers/data.js
@@ -1,18 +1,29 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+require('dotenv').config();
 
 var serviceAccount = require('../public/resources/cgi-elevate-firebase-adminsdk-99kk1-817819ff25.json');
 
+serviceAccount["private_key_id"] = process.env.FIREBASE_ID;
+serviceAccount["private_key"] = process.env.FIREBASE_KEY;
+serviceAccount["client_email"] = process.env.FIREBASE_EMAIL;
+serviceAccount["client_id"] = process.env.FIREBASE_CLIENT_ID;
+serviceAccount["auth_uri"] = process.env.FIREBASE_AUTH_URI;
+serviceAccount["token_uri"] = process.env.FIREBASE_TOKEN_URI;
+serviceAccount["auth_provider_x509_cert_url"] = process.env.FIREBASE_AUTH_CERT;
+serviceAccount["client_x509_cert_url"] = process.env.FIREBASE_CLIENT_CERT;
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cgi-elevate.firebaseio.com"
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
 var db = admin.database();
 var ref = db.ref('db');
 var itemsRef = ref.child('items');
-
-//var storageRef = admin.storage().ref();
-//var imageRef = storageRef.child('images');
 
 /*
 UPLOAD ITEM TO DATABASE
@@ -31,7 +42,6 @@ function addItem(data, callback) {
   // Get key
   var newPostRef = itemsRef.push();
   newPostRef.set(data);
-  console.log(data.pic);
   return callback(data);
 }
 
@@ -48,13 +58,35 @@ function updateItem (key, data, callback) {
   callback(itemsRef.update(updates));
 }
 
-// UPLOAD IMAGE
-function uploadImage(key, img, callback) {
 
+
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Storage  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+mongoose.connect(process.env.MONGOOSE);
+
+var ImageSchema = new Schema({
+  img: { data: Buffer, contentType: String }
+});
+var Image = mongoose.model('Image', ImageSchema);
+
+// UPLOAD IMAGE
+function uploadImage(img, callback) {
+  var image = new Image();
+  image.img.data = fs.readFileSync(img.path);
+  image.img.contentType = img.type;
+  image.save((err, saved) => callback(saved.id));
+}
+
+// RETRIEVE IMAGE
+// returns Image Schema
+function getImage(id, callback) {
+  Image.findById(id, (err, img) => callback(img.img));
 }
 
 module.exports = {
   addItem,
   retrieveItem,
-  updateItem
+  updateItem,
+  uploadImage,
+  getImage
 }
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,36 @@
 const routes = require('express').Router();
-const bodyParser = require('body-parser');
-const multer = require('multer');
 const data = require('../helpers/data');
-var upload = multer();
+const formidable = require('formidable');
+const fs = require('fs');
 
 routes.get('/', function(req, res){
   res.render('index');
 })
 
-routes.use(bodyParser.json());
-routes.use(bodyParser.urlencoded({ extended: true }));
-routes.use(upload.array());
 // SUBMIT ITEM
 routes.post('/submit', function(req, res){
-  console.log(req.form);
-  res.send(req.form);
-//  data.addItem(req.body, e => res.send(e));
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if(err) return res.send('Error reading form!');
+    // if form has image
+    // TODO: implement default image
+    if(files.pic.type.includes('image/')) {
+      data.uploadImage(files.pic, id => {
+        fields["img_id"] = id;
+        data.addItem(fields, e => res.send(e));
+      });
+    } else data.addItem(fields, e => res.send(e));
+  });
 });
 
 // RETRIEVE ITEM
 routes.get('/retrieve/:id', function(req, res) {
-  data.retrieveItem(req.params.id, data => res.send(data));
+  data.getImage(req.params.id, img => {
+    res.contentType(img.contentType);
+    res.end(img.data, 'binary');
+  });
+  //data.retrieveItem(req.params.id, data => res.send(data));
 });
 
 // UPDATE ITEM
