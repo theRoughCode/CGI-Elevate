417bf7ca427bf2a0ecb9692a641732dce68208bd
raphael <raph.koh@gmail.com>
2017-05-12 18:55:09 -0400

    Implement full form retrieval

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,24 @@ routes.post('/submit', function(req, res){
 
 // RETRIEVE ITEM
 routes.get('/retrieve/:id', function(req, res) {
+  data.retrieveItem(req.params.id, field => {
+    data.getImage(field.img_id, img => {
+      res.render('display', {
+        name: field.name,
+        desc: field.desc,
+        qty: field.qty,
+        cat: field.category,
+        bid: field.bid,
+        duration: field.duration,
+        price: field.price,
+        img: `data:${img.contentType};base64,${new Buffer(img.data).toString('base64')}`
+      });
+    });
+  });
+});
+
+// RETRIEVE IMAGE
+routes.get('/img/:id', function(req, res) {
   data.getImage(req.params.id, img => {
     res.contentType(img.contentType);
     res.end(img.data, 'binary');
